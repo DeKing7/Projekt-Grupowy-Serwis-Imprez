f757ebc00f37985217640b320cf1b8b977700438
Damian Kardela <DamKar@gmail.com>
2019-09-08 15:40:22 +0200

    Aktualizacja Rejestracja prawie end na h2 Zrob zadanie 2.8

--- src/main/java/pl/honestit/spring/demo/config/SecurityConfig.java
+++ src/main/java/pl/honestit/spring/demo/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package pl.honestit.spring.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,4 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable();
     }
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT username, password, active FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username = ?");
+    }
+
 }
---
+++ src/main/java/pl/honestit/spring/demo/controllers/LoginControler.java
@@ -0,0 +1,4 @@
+package pl.honestit.spring.demo.controllers;
+
+public class LoginControler {
+}
